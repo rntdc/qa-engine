bb2abf428134b2c0869035c9e1796ad03f11ccf9
Renato <rntdek@gmaail.com>
2020-03-20 17:25:52 -0300

    registro sem hash

--- src/models/user.js
+++ src/models/user.js
@@ -1,9 +1,30 @@
+const bcrypt = require('bcrypt-nodejs');
 module.exports = (sequelize, DataTypes) => {
     const User = sequelize.define('User', {
-        username: DataTypes.STRING,
-        email: DataTypes.STRING,
-        password: DataTypes.STRING
+        username: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            unique: true
+        },
+        email: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            unique: true,
+            validate: {
+                isEmail: true
+            }
+        },
+        password: {
+            type: DataTypes.STRING,
+            allowNull: false
+        }
     });
+
+    /*hash
+    User.hook('beforeCreate', function(user) {
+        user.password = bcrypt.hashSync(user.password, bcrypt.genSaltSync(10), null);
+    });
+    */
 
     return User;
 }
--- src/routes/user/index.js
+++ src/routes/user/index.js
@@ -2,5 +2,6 @@ const express = require('express');
 const router = express.Router();
 
 router.get('/', require('./user-get'));
+router.post('/', require('./user-post'));
 
 module.exports = router;
