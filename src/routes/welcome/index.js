7bd2f73d3398bbdd96667ff953a1c5953f537b7c
Renato <rntdek@gmaail.com>
2020-03-21 01:35:05 -0300

    template welcome

--- src/routes/welcome/index.js
+++ src/routes/welcome/index.js
@@ -2,6 +2,6 @@ const express = require('express');
 const router = express.Router();
 const isAuth = require('./../../middlewares/isAuth');
 
-router.get('/', require('./main'));
+router.get('/', isAuth, require('./home'));
 
 module.exports = router;
