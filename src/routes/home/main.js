1fb5c2dad154ed1e9524c07b891e472528b5c369
Renato <rntdek@gmaail.com>
2020-04-01 17:01:53 -0300

    listando questions e sugestions

--- src/routes/home/main.js
+++ src/routes/home/main.js
@@ -1,3 +1,31 @@
-module.exports = (req, res) => {
-    return res.render('home/index', { user: req.user });
+const User = require('./../../models/index').User;
+const Answer = require('./../../models/index').Answer;
+const Op = require('./../../models/index').Sequelize.Op;
+const sequelize = require('./../../models/index').sequelize;
+
+
+module.exports = async (req, res) => {
+    try{
+        const user = await User.findOne({ where: {username: req.params.username }});
+        const answers = await Answer.findAll({
+            include: [{
+                model: User,
+                as: 'questionUser'
+            }, {
+                model: User,
+                as: 'answerUser'
+            }],
+            where:  {
+                answer_user_id: user.id,
+                answer: { [Op.ne]: '' }
+            }
+        })
+
+        const sugestions = await User.findAll({  limite: 4, order:  sequelize.random(), where: { id: {[Op.notIn]: [req.user.id] }}});
+       
+        return res.render('home/index', { user: req.user, answers, answerUser: user, sugestions });
+    } catch(err){
+        console.log('ERROR', err);
+    }
+    
 }
