45159f007348334f44759e60873f9780b5d7878e
Renato <rntdek@gmaail.com>
2020-04-06 18:01:06 -0300

    exclusao pergunta

--- src/routes/answer/index.js
+++ src/routes/answer/index.js
@@ -3,5 +3,6 @@ const router = express.Router();
 const isAuth = require('./../../middlewares/isAuth');
 
 router.post('/', isAuth, require('./answer-post'));
+router.delete('/:id', isAuth, require('./answer-delete'))
 
 module.exports = router;
